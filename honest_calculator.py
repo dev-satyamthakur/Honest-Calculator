d6ccc877df4b61442e9a4e257a5a6f1bd6a0872b
Neo <sat.thakur20@gmail.com>
2022-07-18 19:43:36 +0530

    Finishing the project and Adding the flow chart image

--- honest_calculator.py
+++ honest_calculator.py
@@ -9,6 +9,9 @@ msg_6 = " ... lazy"
 msg_7 = " ... very lazy"
 msg_8 = " ... very, very lazy"
 msg_9 = "You are"
+msg_10 = "Are you sure? It is only one digit! (y / n)"
+msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+msg_12 = "Last chance! Do you really want to embarrass yourself? (y / n)"
 
 # memory variable
 memory = 0
@@ -52,7 +55,7 @@ while True:
     # setting memory
     if x == "M":
         x = memory
-    elif y == "M":
+    if y == "M":
         y = memory
 
     # trying to convert strings into floats for calculation
@@ -89,7 +92,22 @@ while True:
         store_in_memory = input()
 
         if store_in_memory == "y":
-            memory = result
+            if is_one_digit(result):
+                msg_index = 10
+                while True:
+                    print(eval('msg_' + str(msg_index)))
+                    answer = input()
+
+                    if answer == "y":
+                        if msg_index < 12:
+                            msg_index += 1
+                        else:
+                            memory = result
+                            break
+                    elif answer == "n":
+                        break
+            else:
+                memory = result
             break
         elif store_in_memory == "n":
             break
